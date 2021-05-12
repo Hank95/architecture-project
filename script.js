67a6c3ed2a323227e5cfa506ea23b28c01424587
Henry Pendleton <hhpendleton@gmail.com>
2021-05-11 21:49:05 -0400

    HTML and CSS of main page

---
+++ script.js
@@ -0,0 +1,10 @@
+// const link = document.querySelectorAll(".grid-item");
+// const logo = document.querySelector(".nav");
+
+// link.forEach((el) => {
+//   console.log(el);
+//   el.addEventListener("mouseover", function (e) {
+//     console.log(e);
+//     e.classList.remove(".hidden");
+//   });
+// });
